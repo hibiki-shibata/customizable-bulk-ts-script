01d97eb6a25cba1f6cb401d7f3bcb7b6c19fc81f
hibiki-shibata <hibiki.shibata@wolt.com>
2026-01-22 19:31:14 +0900

    Edit: API request adapt to without body

--- src/api/sendAPIRequest.ts
+++ src/api/sendAPIRequest.ts
@@ -7,15 +7,24 @@ type ApiRequestParams = {
 }
 
 export async function sendAPIRequest(apiRequestParam: ApiRequestParams): Promise<Response> {
-    const response: Response = await fetch(apiRequestParam.URI, {
-        method: `${apiRequestParam.methodType}`,
+
+    const options: RequestInit = {
+        method: apiRequestParam.methodType,
         headers: {
             'Content-Type': 'application/json',
             [apiRequestParam.securityHeaderName]: `${apiRequestParam.accessToken}`
         },
-        body: apiRequestParam.bodyJson ? JSON.stringify(apiRequestParam.bodyJson) : undefined,
         signal: AbortSignal.timeout(5000)
-    }).catch(error => new Response(null, { status: 500, statusText: error.message }))
+    }
+
+    if (apiRequestParam.methodType !== 'GET' && apiRequestParam.methodType !== 'HEAD') {
+        options.body = JSON.stringify(apiRequestParam.bodyJson)
+    }
+
+    const response: Response = await fetch(apiRequestParam.URI, options).catch(error => {
+        return new Response(null, { status: 500, statusText: error.message })
+    })
+    
     if (!response) console.warn(`No response received from ${apiRequestParam.URI}`)
     return response
 }
--- src/service/bulkAction.service.ts
+++ src/service/bulkAction.service.ts
@@ -4,17 +4,17 @@ import { CsvRepository } from '../repository/csvRepository.js'
 import { JsonRepository } from '../repository/jsonRepository.js'
 import { ICsvRepository } from '../type/repository/ICsvRepository.js'
 import { IJsonRepository } from '../type/repository/IJsonRepository.js'
-import { get_list_of_csv_column_names } from '../util/getListOfCsvColumnNames.js'
+import { IBulkActionService } from '../type/service/IBulkAction.service.js'
 import { getFileContent } from '../util/fileReader.js'
 import { PlaceHolderReplacer } from '../util/placeHolderReplacer.js'
 
-export class BulkActionService {
+export class BulkActionService implements IBulkActionService {
     private readonly accessToken: string
     private readonly resource_csv_repository: ICsvRepository
     private readonly resource_json_repository?: IJsonRepository
 
     public constructor() {
-        this.accessToken = getFileContent('./resource/access-token.txt');
+        this.accessToken = getFileContent('./resource/access-token.txt')
         this.resource_csv_repository = CsvRepository.useCsvFileOf(`./resource/${config.csv_file_name}`) // Load CSV file
         this.resource_json_repository = config.json_file_name ? JsonRepository.useJsonFileOf(`./resource/${config.json_file_name}`) : undefined // Load JSON file if specified
     }
@@ -28,7 +28,7 @@ export class BulkActionService {
         for (let i = 0; i < lengthOfCSV; i++) {
             // Replace the name of columns as placeholders in the request URI and JSON body with the value of the optional column for the current row.
             //  e.g. column_name = "Venue Address" request_uri = "https://example.com/[Venue Address]/example" --replace--> https://example.com/Tokyo/example.
-            get_list_of_csv_column_names().forEach(csv_column_name => {
+            this.get_list_of_csv_column_names().forEach(csv_column_name => {
                 const placeHolderReplacer = this.placeHolderReplacerForColumnInRow(i, csv_column_name)
                 request_uri = placeHolderReplacer.applyToUri(request_uri)
                 json_request_body = json_request_body ? placeHolderReplacer.applyToJson(json_request_body) : undefined
@@ -44,7 +44,7 @@ export class BulkActionService {
 
             // Logging failed lines
             if (!res.ok) failed_csv_lines.push((i + 1).toString())
-            this.traceLogger(i, request_uri, res, json_request_body)
+            this.traceLogger(i, res, request_uri, json_request_body)
         }
         console.log("=====🎉All REQUESTS WERE PROCESSED🎉=====\n" + `Failed lines:\n${failed_csv_lines ? failed_csv_lines : 'None'} `)
     }
@@ -55,15 +55,25 @@ export class BulkActionService {
         return PlaceHolderReplacer.placeHolderIs(`[${columnName}]`).replaceWith(cellValue)
     }
 
-    private async traceLogger(rowIndex: number, request_uri: string, apiResponse: Response, json_request_body?: Object): Promise<void> {
-        console.log(`Sending request for CSV row ${rowIndex + 1}...\nReq URI: ${request_uri}\nReq Body: ${JSON.stringify(json_request_body, null, 2)}`)
+    private get_list_of_csv_column_names(): string[] {
+        const optionalColumns: string[] = []
+        Object.entries(config).forEach(([key, value]) => {
+            // Ignore keys that are not additional CSV column names or are empty.
+            if (!key.startsWith("csv_column_name_") || typeof value !== 'string' || !value.trim()) return
+            optionalColumns.push(value.trim())
+        })
+        return optionalColumns
+    }
+
+    private async traceLogger(rowIndex: number, apiResponse: Response, request_uri: string, json_request_body?: Object): Promise<void> {
+        const defaultLogMessage = `Sending request for CSV row ${rowIndex + 1}...\nReq URI: ${request_uri}\nReq Body: ${JSON.stringify(json_request_body, null, 2)}`
         if (!apiResponse.ok) {
-            console.warn(`❌Line ${rowIndex + 1} failed: ${apiResponse.status} ${apiResponse.statusText}`)
+            console.warn(`${defaultLogMessage}\n❌Line ${rowIndex + 1} failed: ${apiResponse.status} ${apiResponse.statusText}`)
         } else if (config.getVenueIDList) {
-            const venueID = await apiResponse.json().then(data => data.results[0].id.$oid).catch(() => console.log("❌Failed to retrieve Venue ID from succeeded response."))
+            const venueID: string = await apiResponse.json().then(data => data.results[0].id.$oid).catch(() => console.log("🚨Failed to retrieve Venue ID from succeeded response."))
             console.log(venueID)
         } else {
-            console.log(`✅ Line ${rowIndex + 1} Succeeed`)
+            console.log(`${defaultLogMessage}\n✅Line: ${rowIndex + 1} Succeeed`)
         }
     }
 }
