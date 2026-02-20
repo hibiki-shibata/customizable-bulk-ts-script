a973cb112e8084e0c5565b7e5dc369d9a82b178f
hibiki-shibata <hibiki.shibata@wolt.com>
2026-02-21 03:52:19 +0900

    Small fix

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -11,7 +11,13 @@ export class Logger {
     public static async log({ rowIndex, apiResponse, request_uri, json_request_body }: LogObj) {
         const defaultLogMessage = `Sent request of CSV row: ${rowIndex + 2}...\nReq URI: ${request_uri}\nReq Body: ${JSON.stringify(json_request_body, null, 2)}`
         // All failed responses will be logged here
-        if (!apiResponse.ok) return console.log(`${defaultLogMessage}\n❌Line ${rowIndex + 2} failed: ${apiResponse.status} ${apiResponse.statusText}`)
+        if (!apiResponse.ok) {
+            console.error(`${defaultLogMessage}`)
+            console.log(`❌Line ${rowIndex + 2} failed: ${apiResponse.status} ${apiResponse.statusText}`)
+            console.log(`Response message: ${await apiResponse.text()}`)
+            return
+        }
+
         if (config.showVenueID) {
             apiResponse.json().then(data => {
                 // const venueID: string = data.results[0].id.$oid
@@ -23,7 +29,7 @@ export class Logger {
                 const cpPaymentBalance: number = data.JPY.total
                 console.log(cpPaymentBalance)
             }).catch((e) => console.log("No Payment balance data"))
-        } else {
+        } else if (apiResponse.ok) {
             console.log(`${defaultLogMessage}\n✅Line: ${rowIndex + 2} Succeeed`)
         }
     }
